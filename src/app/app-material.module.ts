d687b117e63ba36c1d590e64722a7c3048ef51a0
Brandon Miller <bmil_21@yahoo.com>
2020-07-20 18:59:00 -0400

    add material, login button

---
+++ src/app/app-material.module.ts
@@ -0,0 +1,75 @@
+import { NgModule } from '@angular/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSelectModule } from '@angular/material/select';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+@NgModule({
+    imports: [
+        MatSnackBarModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatRadioModule,
+        MatCardModule,
+        MatDialogModule,
+        MatInputModule,
+        MatIconModule,
+        MatMenuModule,
+        MatSidenavModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        MatTabsModule,
+        MatSelectModule,
+        MatListModule,
+        MatPaginatorModule,
+        MatChipsModule,
+        MatExpansionModule,
+        MatButtonToggleModule
+    ],
+    exports: [
+        MatSnackBarModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatRadioModule,
+        MatCardModule,
+        MatDialogModule,
+        MatInputModule,
+        MatIconModule,
+        MatMenuModule,
+        MatSidenavModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        MatTabsModule,
+        MatSelectModule,
+        MatListModule,
+        MatPaginatorModule,
+        MatChipsModule,
+        MatExpansionModule,
+        MatButtonToggleModule
+    ],
+    declarations: [],
+    providers: []
+})
+export class AppMaterialModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app-material.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { LoginComponent } from './pages/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
