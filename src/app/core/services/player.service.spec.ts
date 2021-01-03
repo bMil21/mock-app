13073d434f38a514acb9d99c529129b2e374e7ce
Brandon Miller <bmil_21@yahoo.com>
2021-01-02 20:02:16 -0500

    test player service

--- src/app/core/services/player.service.spec.ts
+++ src/app/core/services/player.service.spec.ts
@@ -1,32 +1,34 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PlayerService } from './player.service';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Player, PlayersResponse } from 'src/app/models/player';
+import { of } from 'rxjs';
 
 describe('PlayerService', () => {
-  let service: PlayerService;
-  let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
+  let playerService: PlayerService;
+  let httpClientSpy: {get: jasmine.Spy};
 
   beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    playerService = new PlayerService(httpClientSpy as any);
+
     TestBed.configureTestingModule({
       imports: [
-        HttpClientModule,
         HttpClientTestingModule
       ],
       providers: [
-        PlayerService
+        PlayerService,
+        {provide: HttpClient, useValue: httpClientSpy}
       ]
     });
-    service = TestBed.inject(PlayerService);
-    httpClient = TestBed.inject(HttpClient);
-    httpTestingController = TestBed.inject(HttpTestingController);
+
+    playerService = TestBed.inject(PlayerService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(playerService).toBeTruthy();
   });
 
   it('should get players', () => {
@@ -47,13 +49,13 @@ describe('PlayerService', () => {
       ]
     };
 
-    service.getPlayers()
+    httpClientSpy.get.and.returnValue(of(mockPlayers));
+
+    playerService.getPlayers()
       .subscribe(players => {
         expect(players[0].name).toEqual("John Doe");
+        expect(players[0]).toBeInstanceOf(Player);
       });
-
-    const req = httpTestingController.expectOne('/assets/data/players.json');
-    expect(req.request.method).toEqual('GET');
-    req.flush(mockPlayers);
+      expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
   });
 });
